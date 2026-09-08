aa83813ddedf21abffcae0a5d6ccf53d9ac783fb
TyuiuRomanovMV <max2002romanov15@gmail.com>
2026-09-08 22:17:42 +0500

    Task4

---
+++ Tyuiu.RomanovMV.Sprint0.Task4.V0.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿namespace Tyuiu.RomanovMV.Sprint0.Task4.V0.Lib
+{
+    public class DataService
+    {
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Sub(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Mul(int a, int b)
+        {
+            return a * b;
+        }
+        public static int Div(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ Tyuiu.RomanovMV.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.RomanovMV.Sprint0.Task4.V0.Lib;
+namespace Tyuiu.RomanovMV.Sprint0.Task4.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(1, 5));
+            Console.WriteLine(DataService.Sub(5, 5));
+            Console.WriteLine(DataService.Mul(2, 5));
+            Console.WriteLine(DataService.Div(50, 5));
+
+            Console.ReadLine();
+        }
+    }
+}
