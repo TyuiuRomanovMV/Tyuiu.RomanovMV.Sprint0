ecc33bc85303bfd269a58276c994da1b1e0805d6
TyuiuRomanovMV <max2002romanov15@gmail.com>
2026-09-08 17:11:07 +0500

    Add project files.

---
+++ Tyuiu.RomanovMV.Sprint0.Task0.V0.Lib/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Tyuiu.RomanovMV.Sprint0.Task0.V0.Lib
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Tyuiu.RomanovMV.Sprint0.Task0.V0/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.RomanovMV.Sprint0.Task0.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
